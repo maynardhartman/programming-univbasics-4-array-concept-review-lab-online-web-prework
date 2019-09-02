b470cc3ad905c3c427deb09f6ede82ea8610d1b1
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-02 08:11:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
 def find_element_index(array, value_to_find)
   # Add your solutio
-  
+  for (element = 0; elment < array.lenght; count++)
+  {
+    if (srray[element])
+  }
   for (count = 0; count < array.lenght; count++)
   {
     if array[count] == value_to_find
